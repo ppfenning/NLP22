6576194dceee7330425eb65bc7a5465167346850
Patrick Pfenning <patrick.pfenning@vtinfo.com>
2022-06-18 12:15:37 -0400

    - rework model objects - add instruction pdf

--- RA1/FakeNews.py
+++ RA1/FakeNews.py
@@ -23,24 +23,38 @@ class FakeNews:
 
     def __run_models(self, col):
         fake, quest = self.__split(col)
+        return {
+            'TF-IDF': self.__get_tfidf(fake, quest),
+            'CV': self.__get_cv(fake, quest)
+        }
+
+    def __get_tfidf(self, fake, quest):
         tf = TfidfVectorizer(
             max_features=self.max_features,
             stop_words='english',
             smooth_idf=True,
             use_idf=True
         )
+        tf.fit(fake)
+        return {
+            'quest_df': pd.DataFrame(tf.transform(quest).todense(), columns=tf.vocabulary_),
+            'idf': pd.Series(tf.idf_, tf.vocabulary_),
+            'vectorizer': tf
+        }
+
+    def __get_cv(self, fake, quest):
         cv = CountVectorizer(
             max_features=self.max_features,
             stop_words='english',
         )
-        models = []
-        for vec in [tf, cv]:
-            vec.fit(fake)
-            models.append(pd.DataFrame(vec.transform(quest).todense(), columns=vec.vocabulary_))
-        return dict(zip(['TF-IDF', 'CountVector'], models))
+        cv.fit(fake)
+        return {
+            'quest_df': pd.DataFrame(cv.transform(quest).todense(), columns=cv.vocabulary_),
+            'vectorizer': cv
+        }
 
     def __split(self, col):
-        df = self.news[[col, 'fake']].copy()
+        df = self.__news[[col, 'fake']].copy()
         return df[df.fake == 'F'][col], df[df.fake == 'Q'][col]
 
     def __join_fold(self, fold):
